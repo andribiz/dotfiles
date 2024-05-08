c5d67110466a84035e3acc65a722382acb4bf364
Andri <andri@bizoftenterprise.com>
2024-05-08 14:46:00 +0700

    add typescript

--- .config/nvim/lua/user/typescript-tools.lua
+++ .config/nvim/lua/user/typescript-tools.lua
@@ -1,3 +1,10 @@
 require("typescript-tools").setup {
 
 }
+
+local opts = { noremap = true, silent = true }
+-- vim.api.nvim_buf_set_keymap(0, "n", "<leader>oi", ":TSToolsOrganizeImports<CR>", opts)
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--     pattern = "*.ts*",
+--     command = ":TSToolsRemoveUnusedImports sync", -- added `sync` here
+-- })
