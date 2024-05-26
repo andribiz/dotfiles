8d98bfee80828cf858777c58f8b755e589cf90cb
Andri <andri@bizoftenterprise.com>
2024-05-26 21:52:00 +0700

    add auto update

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -41,3 +41,16 @@ vim.api.nvim_create_autocmd({ "Colorscheme", "BufReadPre", "BufNewFile" }, {
   group = vim.api.nvim_create_augroup("mycolorscheme", { clear = true }),
   desc = "Set highlight groups for navic",
 })
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = augroup("autoupdate"),
+  callback = function()
+    if require("lazy.status").has_updates then
+      require("lazy").update({ show = false })
+    end
+  end,
+})
