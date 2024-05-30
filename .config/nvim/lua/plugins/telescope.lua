624c4fbc5bd7786746d6aad766a0ef7c31e88b63
Andri <andri@bizoftenterprise.com>
2024-05-30 11:47:00 +0700

    add live_grep_args

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,46 +1,47 @@
 return {
-
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       {
         "nvim-telescope/telescope-live-grep-args.nvim",
-        config = function()
-          LazyVim.on_load("telescope.nvim", function()
-            require("telescope").load_extension("live_grep_args")
-          end)
-        end,
+      },
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        -- enabled = false,
+        build = "make",
       },
     },
     opts = function(_, opts)
       local lga_actions = require("telescope-live-grep-args.actions")
       return vim.tbl_deep_extend("force", opts, {
-        defaults = {
-          extensions = {
-
-            live_grep_args = {
-              auto_quoting = true, -- enable/disable auto-quoting
-              -- define mappings, e.g.
-              mappings = { -- extend mappings
-                i = {
-                  ["<C-k>"] = lga_actions.quote_prompt(),
-                },
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            -- define mappings, e.g.
+            mappings = { -- extend mappings
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
               },
             },
           },
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          },
         },
       })
-      -- opts.defaults.extensions = vim.list_extend(opts.defaults.extensions or {}, {
-      --   live_grep_args = {
-      --     auto_quoting = true, -- enable/disable auto-quoting
-      --     -- define mappings, e.g.
-      --     mappings = { -- extend mappings
-      --       i = {
-      --         ["<C-k>"] = lga_actions.quote_prompt(),
-      --       },
-      --     },
-      --   },
-      -- })
+    end,
+    keys = {
+      { "<leader>/", "<cmd>Telescope live_grep_args<cr>", desc = "Live grep args" },
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      telescope.load_extension("live_grep_args")
+      telescope.load_extension("fzf")
     end,
   },
 }
