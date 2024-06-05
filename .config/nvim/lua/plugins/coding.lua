3acbb451dfcf6cf481ca3498bb6b7b9562071392
Andri <andri@bizoftenterprise.com>
2024-06-06 00:35:02 +0700

    change to neocodium

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -12,16 +12,25 @@ return {
     end,
   },
   {
-    "Exafunction/codeium.nvim",
-    event = "BufEnter",
+    "monkoose/neocodeium",
+    event = "VeryLazy",
     config = function()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set("i", "<C-g>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true, silent = true })
+      local neocodeium = require("neocodeium")
+      neocodeium.setup()
+      vim.keymap.set("i", "<C-g>", neocodeium.accept)
     end,
   },
-
+  -- {
+  --   "Exafunction/codeium.nvim",
+  --   event = "BufEnter",
+  --   config = function()
+  --     -- Change '<C-g>' here to any keycode you like.
+  --     vim.keymap.set("i", "<C-g>", function()
+  --       return vim.fn["codeium#Accept"]()
+  --     end, { expr = true, silent = true })
+  --   end,
+  -- },
+  --
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
