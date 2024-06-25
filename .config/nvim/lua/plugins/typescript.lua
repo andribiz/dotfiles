02b20639c2957d9288ecde437d96455d805077ea
Andri <andri@bizoftenterprise.com>
2024-06-25 13:58:50 +0700

    add better ts error typescript

---
+++ .config/nvim/lua/plugins/typescript.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "OlegGulevskyy/better-ts-errors.nvim",
+    ft = { "typescript", "typescriptreact" },
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = {
+      keymaps = {
+        toggle = "<leader>dd", -- default '<leader>dd'
+        go_to_definition = "<leader>dx", -- default '<leader>dx'
+      },
+    },
+  },
+}
