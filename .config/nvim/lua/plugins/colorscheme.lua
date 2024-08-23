c900a16ff280f52cc600c485dba83b0c5669eb53
Andri <andri@bizoftenterprise.com>
2024-08-23 13:20:42 +0700

    remove colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,58 +11,62 @@ return {
     priority = 1000,
   },
 
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
   -- catppuccin
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-  },
-  {
-    "sainnhe/sonokai",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_transparent_background = "0"
-      vim.g.sonokai_enable_italic = "1"
-      vim.g.sonokai_style = "default"
-    end,
-  },
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = true,
+  --   name = "catppuccin",
+  --   opts = {
+  --     integrations = {
+  --       aerial = true,
+  --       alpha = true,
+  --       cmp = true,
+  --       dashboard = true,
+  --       flash = true,
+  --       gitsigns = true,
+  --       headlines = true,
+  --       illuminate = true,
+  --       indent_blankline = { enabled = true },
+  --       leap = true,
+  --       lsp_trouble = true,
+  --       mason = true,
+  --       markdown = true,
+  --       mini = true,
+  --       native_lsp = {
+  --         enabled = true,
+  --         underlines = {
+  --           errors = { "undercurl" },
+  --           hints = { "undercurl" },
+  --           warnings = { "undercurl" },
+  --           information = { "undercurl" },
+  --         },
+  --       },
+  --       neotest = true,
+  --       neotree = true,
+  --       noice = true,
+  --       notify = true,
+  --       semantic_tokens = true,
+  --       telescope = true,
+  --       treesitter = true,
+  --       treesitter_context = true,
+  --       which_key = true,
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "sainnhe/sonokai",
+  --   lazy = true,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.sonokai_transparent_background = "0"
+  --     vim.g.sonokai_enable_italic = "1"
+  --     vim.g.sonokai_style = "default"
+  --   end,
+  -- },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
