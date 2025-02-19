6987d83fdac2a8afac02f8ab0d1c8be3ebaae84b
Andri <andri@bizoftenterprise.com>
2025-02-19 17:06:45 +0700

    remove: fzf

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -99,9 +99,25 @@ return {
       },
       picker = {
         layout = {
-          preset = "telescope",
+          preset = "default",
         },
         layouts = {
+          default = {
+            layout = {
+              box = "horizontal",
+              width = 0.8,
+              min_width = 120,
+              height = 0.8,
+              {
+                box = "vertical",
+                border = "rounded",
+                title = "{title} {live} {flags}",
+                { win = "input", height = 1, border = "bottom" },
+                { win = "list", border = "none" },
+              },
+              { win = "preview", title = "{preview}", border = "rounded", width = 0.55 },
+            },
+          },
           telescope = {
             reverse = true,
             layout = {
